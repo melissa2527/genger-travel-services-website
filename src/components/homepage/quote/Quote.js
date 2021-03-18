664f5c2daddbcc764a384a2153fd555f10bd1e40
annie2527 <you@example.com>
2021-03-18 02:04:27 +0100

    quote container

--- src/components/homepage/quote/Quote.js
+++ src/components/homepage/quote/Quote.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './quote.scss';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 export const Quote = () => {
+
+    useEffect(() => {
+        Aos.init({duration: 1500})
+    }, [])
+
     return (
         <div className='quote-container'>
-            <div className='quote'>
+            <div className='quote' data-aos='fade-up'>
                 <h5>"The world is a book and those who do not travel, read only one page."</h5>
                 <p>St. Augustine</p>
+            </div>
+            <div className='btn' data-aos='fade-up'>
+                <button>Get Ideas</button>
             </div>
         </div>
     )
